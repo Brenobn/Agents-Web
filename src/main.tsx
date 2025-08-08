64d9f56ff0b6dfe94c4a1285e1f96a21d2d9ddb9
Brenobn <brenobragabn@gmail.com>
2025-08-08 03:58:27 -0300

    Adding a biome suppression to fix complaints

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './app'
 
+// biome-ignore lint/style/noNonNullAssertion: mandatory by React
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
